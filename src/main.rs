bec54580292835f53e79b970c75fa54b51aef732
Gl4SS3S <jthurling78@gmail.com>
2024-06-21 19:35:32 +0200

    Added lookup for 2 arrays

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,20 @@
-fn main() {}
+fn lookup(first_arr: &[i32; 5], second_arr: &[i32; 5], key: i32) -> bool {
+    let mut contains_key: bool = false;
+
+    for i in 0..5 {
+        if first_arr[i] == key || second_arr[i] == key {
+            contains_key = true;
+        }
+    }
+
+    contains_key
+}
+
+fn main() {
+    let first_cuckoo_arr: [i32; 5] = [0; 5];
+    let second_cuckoo_arr: [i32; 5] = [0; 5];
+
+    if lookup(&first_cuckoo_arr, &second_cuckoo_arr, 0) {
+        return;
+    }
+}
